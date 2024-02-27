c6986b9daa8ecc1f32ade5b42ef0583b50b26037
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-27 17:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function mincost(arr)
 { 
+	arr.sort((a,b)=>a-b);
+
+	let totalCost = 0;
+
+	while(arr.length>1){
+	let rope1 = arr.shift()
+	let rope2 = arr.shift()
+		let combinedRope = rope1+rope2
+		totalCost += combinedRope
+		arr.push(combinedRope)
+		arr.sort((a,b)=>a-b);
+		
+	}
+	return totalCost
 //write your code here
 // return the min cost
   
